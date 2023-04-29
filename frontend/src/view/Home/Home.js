cec221f205511fd108bf5d7d9a057837e6143d8d
unknown <mariea.anjuman.shrestha@g.bracu.ac.bd>
2023-04-29 10:01:51 +0600

    add frontend

---
+++ frontend/src/view/Home/Home.js
@@ -0,0 +1,58 @@
+import React, { Fragment, useEffect } from "react";
+//import { CgMouse } from "react-icons/all";
+import "./Home.css";
+import ProductCard from "./ProductCard.js";
+import MetaData from "../layout/MetaData";
+import { clearErrors, getProduct } from "../../actions/productAction";
+import { useSelector, useDispatch } from "react-redux";
+import Loader from "../layout/Loader/Loader";
+import { useAlert } from "react-alert";
+
+const Home = () => {
+  const alert = useAlert();
+  const dispatch = useDispatch();
+  const { loading, error, products } = useSelector((state) => state.products);
+
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+    dispatch(getProduct());
+  }, [dispatch, error, alert]);
+
+  return (
+    <Fragment>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <MetaData title="Technoids" />
+
+          <div className="banner">
+            {/* <p>Welcome to Technoids</p> */}
+            <h1>BUY YOUR DREAM LAPTOP FROM US</h1>
+            
+
+            {/* <a href="#container">
+              <button>
+                Scroll <CgMouse />
+              </button>
+            </a> */}
+          </div>
+
+          <h2 className="homeHeading">Featured Products</h2>
+
+          <div className="container" id="container">
+            {products &&
+              products.map((product) => (
+                <ProductCard key={product._id} product={product} />
+              ))}
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
+
+export default Home;
---
+++ frontend/src/view/layout/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer id="footer">
+      <div className="midFooter">
+        <h1>Technoids.</h1>
+
+        <p>Copyrights 2022 &copy; Technoids and Engineering | All rights reserved</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
